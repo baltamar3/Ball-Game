00041bf1039fcb683a868757755a9d1faf6139ec
brayan <brayan3288@gmail.com>
2020-03-29 15:08:06 -0500

    Controller de la camara ok

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float movVertical, movHorizontal;
+    public float velocidad = 1.0f;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movVertical = Input.GetAxis("Vertical");
+        movHorizontal = Input.GetAxis("Horizontal");
+        print(movHorizontal);
+
+        Vector3 movimiento = new Vector3(movHorizontal, 0.0f ,movVertical); 
+
+        rb.AddForce(movimiento*velocidad);
+    }
+}
