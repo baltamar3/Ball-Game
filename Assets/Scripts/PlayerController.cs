78b2ce2b010a24ff254a2a9700069d0b9019b8f0
brayan <brayan3288@gmail.com>
2020-03-29 16:23:29 -0500

    Contador de gams funcionando

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     float movVertical, movHorizontal;
     public float velocidad = 1.0f;
+    public float altitud = 8.0f;
+    public bool isJump = false;
+    public Text gemsText;
+    public Text lifesText;
+    int gems = 0;
+    int lifes = 3;
 
     private Rigidbody rb;
     // Start is called before the first frame update
@@ -15,7 +22,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         movVertical = Input.GetAxis("Vertical");
         movHorizontal = Input.GetAxis("Horizontal");
@@ -24,5 +31,28 @@ public class PlayerController : MonoBehaviour
         Vector3 movimiento = new Vector3(movHorizontal, 0.0f ,movVertical); 
 
         rb.AddForce(movimiento*velocidad);
+
+        if(Input.GetKey(KeyCode.Space) && (!isJump)){
+            Vector3 salto = new Vector3(0,altitud,0);
+            rb.AddForce(salto*velocidad);
+            isJump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){
+            print(collision.gameObject.name);
+            if (collision.gameObject.name == "Floor" || collision.gameObject.name == "Wood")
+            {
+                isJump = false;
+            }
+    }
+
+    void OnTriggerEnter(Collider collider){
+            if (collider.gameObject.name == "Gema")
+            {
+                Destroy(collider.gameObject);
+                gems += 1;
+                gemsText.text ="0"+gems.ToString();
+            }
     }
 }
